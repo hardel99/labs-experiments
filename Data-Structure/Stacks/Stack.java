7944e4e23af6bb66af4e7f3c9ebbb8c39d2c7b1c
Harold Delgado <34784651+hardel99@users.noreply.github.com>
2019-09-16 10:55:39 -0600

    Create Stack.java

---
+++ Data-Structure/Stacks/Stack.java
@@ -0,0 +1,53 @@
+package prub;
+
+/**
+ *
+ * @author hardel
+ */
+public class Pila {
+    private int tope = -1;
+    private final int[] vec;
+
+    public Pila(int max) {
+        vec = new int[max];
+    }
+    
+    public boolean isFull() {
+        return tope == vec.length - 1;
+    }
+    
+    public boolean isEmpty() {
+        return tope == -1;
+    }
+    
+    public void push(int element) {
+        if(!isFull()) {
+            vec[++tope] = element;
+        } else{
+            System.err.println("Stack OVERFLOW!!! xD");
+        }
+    }
+    
+    public int pop() {
+        int topElement;
+        
+        if(!isEmpty()) {
+            topElement = vec[tope--];
+        } else{
+            System.err.println("Can't pop on an empty stack, please enter data to the stack first");
+            topElement = -1;
+        }
+        
+        return topElement;
+    }
+    
+    public void printStack() {
+        if(!isEmpty()) {
+            for(int i: vec) {
+                System.out.println("Dato de la pila:" + i);
+            }
+        } else{
+            System.err.println("Can't print an empty stack, please enter data to the stack first");
+        }
+    }
+}
